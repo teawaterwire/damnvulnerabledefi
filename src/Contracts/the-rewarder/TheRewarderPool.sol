b170e226752e39cc327dad39c278db68f7e08ff6
teawaterwire <admin@teawaterwire.dev>
2022-03-10 09:28:03 +0100

    what a good start

--- src/Contracts/the-rewarder/TheRewarderPool.sol
+++ src/Contracts/the-rewarder/TheRewarderPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity >=0.8.10;
 
 import {RewardToken} from "./RewardToken.sol";
 import {DamnValuableToken} from "../DamnValuableToken.sol";
--- src/Contracts/unstoppable/UnstoppableLender.sol
+++ src/Contracts/unstoppable/UnstoppableLender.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity >=0.8.10;
 
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
